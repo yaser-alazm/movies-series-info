637de9975eb5e525e6192660937310ef2c9b4d4d
yaser-alazem <yaser.azem@hotmail.com>
2019-02-07 22:41:50 +0200

    Updates

---
+++ precache-manifest.0030a2e661ed9c8e5801fb7bba8c0030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7920a5aea1706981845b",
+    "url": "/movies-series-info/static/js/runtime~main.7920a5ae.js"
+  },
+  {
+    "revision": "d002beeeec97e0fd6f8c",
+    "url": "/movies-series-info/static/js/main.d002beee.chunk.js"
+  },
+  {
+    "revision": "94369a59a5b524149e42",
+    "url": "/movies-series-info/static/js/1.94369a59.chunk.js"
+  },
+  {
+    "revision": "d002beeeec97e0fd6f8c",
+    "url": "/movies-series-info/static/css/main.cbabb3fa.chunk.css"
+  },
+  {
+    "revision": "a15b128727d1e141b30d5d792756a86b",
+    "url": "/movies-series-info/index.html"
+  }
+];
